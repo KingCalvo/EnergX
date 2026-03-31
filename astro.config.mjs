9602a34db4eae406251256dfb723f7c98f444a05
Enrique Calvo Garcia <enriquecalvo75@gmail.com>
2026-03-31 11:38:49 -0600

    fix: remove vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,8 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
-import vercel from "@astrojs/vercel";
 
 export default defineConfig({
   integrations: [react()],
-  adapter: vercel(),
   image: {
     service: {
       entrypoint: "astro/assets/services/noop",
