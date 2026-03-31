1a01cd05492a903b53cddc43fd19025d9a1030a6
Enrique Calvo Garcia <enriquecalvo75@gmail.com>
2026-03-31 12:31:14 -0600

    cambios en astro config import de tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
-  integrations: [react()],
+  integrations: [react(), tailwind()],
   image: {
     service: {
       entrypoint: "astro/assets/services/noop",
